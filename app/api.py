4794839e9586002c6f537b6689e5e6b85f85b794
Derrick Kipchumba <dkip64@gmail.com>
2018-03-27 05:15:31 +0300

    [finishes#156295376]Setting up create user endpoint

---
+++ app/api.py
@@ -0,0 +1,54 @@
+from flask import Flask,request,jsonify
+from flask_api import FlaskAPI
+import uuid #generates random public id
+from werkzeug.security import generate_password_hash, check_password_hash
+from classes import User
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='siri'
+
+
+@app.route('/book',methods=['POST'])
+def add_a_book():
+    return ''
+
+@app.route('/api/books/<bookId>',methods=['PUT'])
+def modify_book():
+    return ''
+
+@app.route('/api//books/<bookId>',methods=['DELETE'])
+def remove_a_book():
+    return ''
+
+@app.route('/api/books/<bookId>',methods=['GET'])
+def get_books():
+    return ''
+
+@app.route('/api/users/books/<bookId>',methods=['POST'])
+def borrow_book():
+    return ''
+
+@app.route('/api/auth/register',methods=['POST'])
+def create_user():
+    """endpoint for creating a user"""
+    data=request.get_json()
+    hashed_password=generate_password_hash(data['password'],method='sha256')
+    new_user=User()
+    new_user.public_id=str(uuid.uuid4())
+    new_user.name=data['name']
+    new_user.password=hashed_password
+    new_user.admin=False
+    # response.status_code = 201
+    return jsonify(new_user.__dict__)
+
+@app.route('/api/auth/login',methods=['POST'])
+def login():
+    return ''
+
+@app.route('/api/auth/reset-password',methods=['POST'])
+def reset_password():
+    return ''
+
+if __name__=='__main__':
+    app.run(debug=True)
